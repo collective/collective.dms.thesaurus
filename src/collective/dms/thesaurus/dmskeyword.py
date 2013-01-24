edc2271e0442ac163977de598178dd2439a764df
David Convent <davconvent@bruxxel.org>
2013-01-24 11:48:39 +0100

    changed widget for equivalences to DataGrid

--- src/collective/dms/thesaurus/dmskeyword.py
+++ src/collective/dms/thesaurus/dmskeyword.py
@@ -1,11 +1,10 @@
 #import datetime
-from zope.interface import implements, implementer
+from zope.interface import implements, implementer, Interface
 #from zope.component import adapts
 from zope.component import adapter
 
 from zope import schema
 from zope.schema.interfaces import IList
-
 
 from z3c.form.interfaces import IFormLayer, IFieldWidget, IMultiWidget
 from z3c.form.widget import FieldWidget, MultiWidget
@@ -15,6 +14,9 @@ from plone.dexterity.schema import DexteritySchemaPolicy
 from plone.dexterity.content import Item
 
 from plone.supermodel import model
+from plone.autoform.directives import widget
+
+from collective.z3cform.datagridfield import DataGridFieldFactory, DictRow
 
 from . import _
 from .relatedkeywords import RelatedThesaurusKeywords
@@ -36,16 +38,23 @@ class Equivalences(schema.List):
 def EquivalencesWidget(field, request):
     return FieldWidget(field, MultiWidget(request))
 
+class IEquivalencesSchema(Interface):
+    equiv = schema.TextLine(
+            title=_("Equivalence"),
+#            required=False,
+            )
+
 
 class IDmsKeyword(model.Schema):
     """ """
 
-    # XXX: Ungly widget that needs to be replaced
-    equivs = Equivalences(
-        title=u'EQs',
+    equivs = schema.List(
+        title=_(u"EQ (Equivalences)"),
         required=False,
-        value_type=schema.TextLine()
+        value_type=DictRow(title=_("Equivalence"),
+                           schema=IEquivalencesSchema)
         )
+    widget(equivs=DataGridFieldFactory)
 
     # BT: broader term
     broader = RelatedThesaurusKeywords(
